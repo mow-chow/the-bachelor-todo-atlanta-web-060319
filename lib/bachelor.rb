e4594e9ef349a41e8db08a3600f0abe47605cdc3
Catherine Cherubin <cat.cherubin@gmail.com>
2019-05-31 18:49:10 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,40 @@
-def get_first_name_of_season_winner(data, season)
-  # code here
+def get_first_name_of_season_winner(data,season)
+ data[season.to_sym].each do |into_array|
+   if into_array[:status] =="Winner"
+     return into_array[:name].split(" ")[0]
+   end
+ end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+data.each do |season, season_array|
+  season_array.each do |into_array|
+    if into_array[:occupation] == occupation
+      return into_array[:name]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+ data.each do |season, season_array|
+    season_array.each do |into_array|
+      if into_array[:hometown] == hometown
+        counter+=1
+      end
+  end
+ end
+ return counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, season_array|
+    season_array.each do |into_array|
+      if into_array[:hometown] == hometown
+        return into_array[:name]
+      end
+  end
 end
 
-def get_average_age_for_season(data, season)
-  # code here
-end
+get_occupation(data,"New York, New York")
